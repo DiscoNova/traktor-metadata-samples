99c1f341c815c4d93095680d3bef99ed9a132ba4
Markku Uttula <markku.uttula@cleverest.eu>
2019-10-25 02:34:05 +0300

    Some comments

--- python/client.py
+++ python/client.py
@@ -1,6 +1,9 @@
 from requests import get
 from time import sleep
 
+# This script attempts to get the current data from the API endpoint ... if it gets something and the data is
+# different from "what it was before", the data will get overwritten.
+
 while True:
     response = get('http://127.0.0.1:8080/api/00000000-0000-0000-0000-000000000000/current')
     if response.status_code == 200:
@@ -9,3 +12,5 @@ while True:
             if old <> response.content:
                 file.write(response.content)
     sleep(1)
+
+    # If the fetch fails (for whatever reason) the script will continue to fire requests at the server
