ec42bf0fabe364bd582d1cdb3fa7a498d2397566
Markku Uttula <markku.uttula@cleverest.eu>
2020-05-02 22:12:42 +0300

    Parse JSON into a TXT-file

--- python/client.py
+++ python/client.py
@@ -1,6 +1,8 @@
 from requests import get
 from time import sleep
 
+import json
+
 # This script attempts to get the current data from the API endpoint ... if it gets something and the data is
 # different from "what it was before", the data will get overwritten.
 
@@ -11,6 +13,11 @@ while True:
             old = file.read()
             if old <> response.content:
                 file.write(response.content)
+                data = json.loads(response.content)
+                if 'track' in data:
+                    text = open('current.txt', 'w+')
+                    # Note: this does not handle non-ASCII characters properly...
+                    text.write(data.get('track').get('artist') + ' - ' + data.get('track').get('title'))
     sleep(1)
 
     # If the fetch fails (for whatever reason) the script will continue to fire requests at the server
